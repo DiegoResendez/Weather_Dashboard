01fee9b59ede7d2525360139103993b71c299851
unknown <diego.resendez@yahoo.com>
2020-07-07 23:03:51 -0700

    Final Commit-script and index

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 
 
-
-//I want to combine document.getElementByID() and 
-// document.getElementById("The newCity").style.textTransform = "uppercase";
-
 const userInput = document.getElementById("userInput");
 const cityElement = document.getElementById("searchedCity");
 const weatherIconElement = document.getElementById("weather-icon");
@@ -11,13 +7,23 @@ const temperatureElement = document.getElementById("temperature");
 const humidityElement = document.getElementById("humidity");
 const windSpeedElement = document.getElementById("windSpeed");
 const indexUVElement = document.getElementById("indexUV");
+const previousSearchesElement = document.getElementById("previousSearchList");
 
 
 
 let city = "";
 let lat, long;
 let previousSearches = [];
-
+let cityArray = JSON.parse(localStorage.getItem("searches"));
+if(cityArray){
+  previousSearches = cityArray;
+  for(let i = 0; i < previousSearches.length; i++) {
+    let node = document.createElement("li");
+    node.classList.add("list-group-item");
+    node.innerHTML = previousSearches[i];
+    previousSearchesElement.appendChild(node);
+    }
+}
 
 searchButton.addEventListener("click", fiveDayForecastSearch);
 searchButton.addEventListener("click", searchUserInput);
@@ -25,7 +31,21 @@ searchButton.addEventListener("click", searchUserInput);
 
 function searchUserInput() {  //City Search Function
   event.preventDefault();
-  let searchCity = $("#userInput").val();
+  let searchCity = $("#userInput").val().toUpperCase();
+  if(previousSearches.length == 10){
+    previousSearches.shift();
+    previousSearchesElement.removeChild(previousSearchesElement.getElementsByTagName("li")[0]);           // Remove <ul>'s first child node (index 0)
+    console.log(previousSearchesElement);
+  };
+  previousSearches.push(searchCity);
+  localStorage.setItem("searches", JSON.stringify(previousSearches));
+
+     let node = document.createElement("li");
+    node.innerHTML = searchCity;
+    node.classList.add("list-group-item");
+    previousSearchesElement.appendChild(node); 
+  // console.log(previousSearches);
+
   let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=9df96d10ddb6902ee29290be45dda446&units=imperial`;
 
   $.ajax({
@@ -34,17 +54,17 @@ function searchUserInput() {  //City Search Function
   }).then(function (response) {
     // console.log(response);
     cityElement.innerHTML = searchCity.toUpperCase();
-    document.getElementById("past-search-0").innerHTML = searchCity.toUpperCase();
 
     $("#weather-icon").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
 
     lat = response.coord.lat;
     long = response.coord.lon;
 
+UVIndex(lat, long);
+
     temperatureElement.innerHTML = `Temperature: ${response.main.temp} F`;
     humidityElement.innerHTML = `Humidity: ${response.main.humidity}`;
     windSpeedElement.innerHTML = `Wind Speed: ${response.wind.speed}`;
-    indexUVElement.innerHTML = `UV Index: `;
   });
 };
 
@@ -52,15 +72,12 @@ function fiveDayForecastSearch() { //5-Day Forecast Function
   event.preventDefault();
   let fiveDayForecast = $("#userInput").val();
   let queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${fiveDayForecast}&appid=9df96d10ddb6902ee29290be45dda446&units=imperial`;
-
+  
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-    // console.log(response);
-
-
-
+      
     for (let i = 0; i < 5; i++) {
       let day = '.day-' + (i + 1);
       // console.log(day);
@@ -78,88 +95,31 @@ function fiveDayForecastSearch() { //5-Day Forecast Function
       $(forecastDay[3]).text("Humidity: " + response.list[i * 8].main.humidity);
     }
   });
-  addToCityList();
-  previousSearchList();
-  // getPosition();
-  // UVIndex();
 };
 
-
-// }
-// get input value push input to array
-// set value in array
-//innerhtml
-function addToCityList() { // function to keep the array list for local storage
-  console.log("Add to City List")
-
-  previousSearches = JSON.parse(localStorage.getItem("searches"));
-  if (previousSearches) {
-    for (let i = 0; i < previousSearches.length; i++) {
-      $('#past-search-' + i).text(previousSearches[i]);
-    }
-  }
-}
-
-
-function previousSearchList() {
-  console.log(previousSearches);
-  // let previousSearchList = localStorage.getItem("previousSearches");
-  
-  // var json = JSON.stringify(previousSearches);
-  // localStorage.setItem("searches", json);
-
-  // if(previousSearches){
-  //   previousSearches = JSON.parse(previousSearches);
-  //   previousSearches.push(previousSearchList);
-  // }
-
-  // let cityList = document.getElementById("past-search-[i]").innerHTML;
-
-  // for(let i = 0; i < cityList.length; i++) {
-  //   let node = document.createElement("li");
-  //   let textnode = document.createTextNode(cityList);
-  //   node.appendChild(textnode);
-  // }
-}
-
-
-
-  function getPosition(lat, long) {
-    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="
-      + lat + "&lon=" + long + "&appid=9df96d10ddb6902ee29290be45dda446";
-    console.log("Getting Lat and Long");
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function (response) {
-      console.log(response);
-    });
-  }
-
-  function UVIndex() {
-    console.log("UVIndex");
+  function UVIndex(lat, long) {
     let queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat="
-      + lat + "&lon=" + long + "&appid=9df96d10ddb6902ee29290be45dda446";
-
+    + lat + "&lon=" + long + "&appid=9df96d10ddb6902ee29290be45dda446";
+    
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function (response) {
-      console.log(response);
-
+      console.log(response, "UVIndex");
+      
       //console.log("UVI: " + response.value);
-
+      indexUVElement.innerHTML = `UV Index: ${response.value}`;
+      
       let uvi = response.value;
       let uviFloor = Math.floor(uvi);
       let cityDiv = $(".city").children();
-
+      
       let low = "badge-success";
       let mod = "badge-warning";
       let hi = "badge-hi"; // css class bg-orange!
       let vhi = "badge-danger";
       let extreme = "badge-extreme"; // css class bg-purple
-
+      
       if (uviFloor <= 2) {
         $(cityDiv[4]).html("<p mb-0>UV Index: " + "<span class=\"text-white badge " + low + "\" + style=\"font-size: 16px;\">" + uvi + "</span></p>");
       }
@@ -177,11 +137,3 @@ function previousSearchList() {
       }
     });
   }
-
-
-
-
-
-
-
-
